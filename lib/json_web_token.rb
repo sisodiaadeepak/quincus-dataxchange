738601d5281b74d6d54bdbf7745d9af9560ec632
Deepak Kumar <deepak.enbake@gmail.com>
2019-02-01 19:01:24 +0530

    make module instead of class

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,15 +1,13 @@
-class JsonWebToken
- class << self
-   def encode(payload, exp = 24.hours.from_now)
-     payload[:exp] = exp.to_i
-     JWT.encode(payload, Rails.application.credentials.secret_key_base)
-   end
+module JsonWebToken
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, Rails.application.credentials.secret_key_base)    
+  end
 
-   def decode(token)
-     body = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
-     HashWithIndifferentAccess.new body
-   rescue
-     nil
-   end
- end
+  def self.decode(token)
+    body = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+    HashWithIndifferentAccess.new body
+  rescue
+    nil
+  end
 end
