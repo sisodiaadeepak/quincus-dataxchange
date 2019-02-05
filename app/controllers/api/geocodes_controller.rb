9b693ae03148b245d4c019f3e4d3ff26c59a5aea
Deepak Kumar <deepak.enbake@gmail.com>
2019-02-05 12:39:17 +0530

    api to get flatshiments

--- app/controllers/api/geocodes_controller.rb
+++ app/controllers/api/geocodes_controller.rb
@@ -8,6 +8,7 @@ class Api::GeocodesController < ApplicationController
 				csv_text = params[:geocode_file].tempfile
 				csv = CSV.parse(csv_text, :headers => true)
 			
+				@batch_id = SecureRandom.uuid
 				csv.each do |row|
 					# Remove key value pair which contain nil and unnecessary values
 					flatshipment_params = row.to_hash.reject { |k,v| v.nil? }
@@ -19,7 +20,6 @@ class Api::GeocodesController < ApplicationController
 					flatshipment_params.to_hash.each_pair do |k,v|
 						flatshipment_new_params.merge!({k.downcase => v}) 
 					end
-					@batch_id = SecureRandom.uuid
 					flatshipment_new_params.merge!("batch_id"=>"#{@batch_id}", "organisation_id" => "#{current_user.organisation.id}")
 
 					FlatShipment.create!(flatshipment_new_params)
@@ -29,15 +29,32 @@ class Api::GeocodesController < ApplicationController
 					    batch_id: "#{@batch_id}"
 					  }.to_json
 			else
-				render status: 200, json: {
-					    message: "File Not Found"
-					  }.to_json
+				render  json: {
+					    	message: "File Not Found"
+						}.to_json
 			end
 		rescue Exception => e
 
-			render status: 200, json: {
+			render json: {
 					    message: "#{e}"
+					}.to_json
+		end
+	end
+
+
+	def get_flatshipment
+		begin
+			if params[:batch_id].present?
+				@flatshipments = FlatShipment.where(batch_id: params[:batch_id])
+				render status: 200, json: {
+					    message: "success",
+					    flatshipments: "#{@flatshipments.to_json}"
 					  }.to_json
+			end
+		rescue Exception => e
+			render json: {
+						message: "#{e}",
+					}.to_json
 		end
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 root to: 'quincus_dataxchanges#index'
 
 post 'api/geocodes/geocode_file', to: 'api/geocodes#geocode_file'
+get 'api/geocodes/geocode_file/:batch_id', to: 'api/geocodes#get_flatshipment'
 
 get 'get_users', to: 'quincus_dataxchanges#get_users'
 post 'authenticate', to: 'user_authentication#authenticate'
