cf96b00e8ba944be02050540f94d80a56ec52553
Deepak Kumar <deepak.enbake@gmail.com>
2019-02-08 15:36:24 +0530

    validate address field

--- app/models/flat_shipment.rb
+++ app/models/flat_shipment.rb
@@ -2,4 +2,5 @@ class FlatShipment < ApplicationRecord
 	belongs_to :organisation
 	belongs_to :job, dependent: :destroy, optional: true
 	belongs_to :vehicle, optional: true
+	validates :delivery_address, presence: true
 end
